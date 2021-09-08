0ebd2b832efb33709168da4f7f54fb021a179f16
FukudaTaiga <fuku.taiga.0119@gmail.com>
2021-09-09 00:19:56 +0900

    init

---
+++ hello_world/hello_world.rs
@@ -0,0 +1,11 @@
+/** 
+* fn - function
+* main - 常に最初に呼ばれる
+*/
+fn main() {
+    // always 4 indent
+    // 世 界 よ 、 こ ん に ち は
+    //println() -> function call, println!() -> macro call.
+    //!が重要. ;で行末.
+    println!("Hello, world!");
+}
